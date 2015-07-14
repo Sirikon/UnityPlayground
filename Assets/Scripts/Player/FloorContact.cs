95417e026e1f098a0c40f9a98cbbcb5efa418f59
Carlos Fernández Llamas <sirikon2@gmail.com>
2015-07-14 04:35:17 +0200

    First commit

---
+++ Assets/Scripts/Player/FloorContact.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorContact : MonoBehaviour {
+
+    public bool isTouching = false;
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        isTouching = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collider) {
+        isTouching = false;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    [Range(0.0f, 100.0f)]
+    public float movementSpeed;
+    [Range(0.0f, 50.0f)]
+    public float jumpForce;
+
+    private Rigidbody2D rigidbody2D;
+    private FloorContact floorContact;
+    private Animator animator;
+
+    void Awake() {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        floorContact = transform.FindChild("FloorContact").gameObject.GetComponent<FloorContact>();
+        animator = GetComponent<Animator>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+        if (CanJump()) {
+            Jump();
+        }
+	}
+
+    void Move() {
+
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        transform.Translate((Vector3.right * horizontalAxis * movementSpeed) * Time.deltaTime);
+
+        if (horizontalAxis != 0){
+            animator.SetBool("Walking", true);
+        } else {
+            animator.SetBool("Walking", false);
+        }
+
+        if (horizontalAxis < 0) {
+            transform.localScale = new Vector3(-1, 1, 1);
+        } else if(horizontalAxis > 0) {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+    }
+
+    void Jump() {
+        if (Input.GetButtonDown("Jump")) {
+            rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    bool CanJump() {
+        return floorContact.isTouching;
+    }
+}
